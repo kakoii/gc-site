0405ef324129f5a9efb33d16aec5943520f285b4
kakoii <jaycehollon@gmail.com>
2023-02-28 09:36:59 -0600

    [fix] fixed white page error

---
+++ src/pages/Landing.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing(){
+    return(
+        <div class="Landing">
+        <div class="header">
+    <nav>
+      <a href="/about">About</a>
+      <a href="/minecraft">Minecraft</a>
+      <a href="/theboys">The Boys</a>
+    </nav>
+    </div>
+    <div>
+        <h1>welcome to cumsocks.wtf </h1>
+        <h2>navigate using the header (currently WIP, only header item that works is Minecraft)</h2>
+        <img src="https://thumbs.gfycat.com/EnchantedDimJohndory-max-1mb.gif"></img> 
+        <footer>
+        ©cumsocks.wtf 2023
+        </footer>
+    </div>
+    </div>
+
+
+    )
+}
+
+
+export default Landing;
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Main.css';
+
+function Main(){
+    return(
+        <div>
+            <a  data-text="&nbsp;ENTER COMMUNITY&nbsp;" href="/login">&nbsp;ENTER COMMUNITY&nbsp;</a>
+        </div>
+    )
+}
+
+
+export default Main;
