217fdcdf7abee4f9f1be0e0563624f137137b6c1
kakoii <jaycehollon@gmail.com>
2023-03-07 09:57:17 -0600

    [fix] switched to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from '../src/reportWebVitals';
-import { BrowserRouter, Routes, Route, Link, HashRouter } from 'react-router-dom'
+import {Route, Link, HashRouter } from 'react-router-dom'
 import Main from '../src/pages/Main.js'
 import Login from '../src/pages/Login.js'
 import Landing from '../src/pages/Landing.js'
@@ -12,18 +12,15 @@ import TheBoys from '../src/pages/TheBoys.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <HashRouter basename="/">
-  <Routes>
-    <Route index element={<Main />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/landing" element={<Landing />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/minecraft" element={<Minecraft />} />
-    <Route path="/theboys" element={<TheBoys />} />
-  </Routes>
+    <HashRouter>
+      <div>
+    <Route path="/" component={Main} />
+    <Route path="/login" component={Login} />
+    <Route path="/landing" component={Landing} />
+    <Route path="/about" component={About} />
+    <Route path="/minecraft" component={Minecraft} />
+  </div>
   </HashRouter>
-  </BrowserRouter>
 
 );
 
